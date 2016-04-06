ce1d7dd214c9684a346748da291900ec94fe16d3
getinkshedtears <clairer86@gmail.com>
2016-04-06 21:49:13 +0000

    updated callbacks

--- oauth.js
+++ oauth.js
@@ -2,13 +2,13 @@ var ids = {
   facebook: {
     clientID: '495924703942654',
     clientSecret: '03482617e61200a46c31092f05a8714f',
-    callbackURL: 'https://fccvotingapp-getinkshedtears.c9users.io/auth/facebook/callback'
+    callbackURL: 'http://fast-taiga-47790.herokuapp.com/auth/facebook/callback'
   },
   
   twitter : {
     consumerKey: 'WHj8xFG8JbELKTiEjfqJQKoJX',
     consumerSecret: 'HyNj6f74q2X6cp2wEGrcWCNuJfLmjnTOJu97j3kJKQYggVu8ju',
-    callbackURL: 'https://fccvotingapp-getinkshedtears.c9users.io/auth/twitter/callback'
+    callbackURL: 'http://fast-taiga-47790.herokuapp.com/auth/twitter/callback'
   },
   
   
